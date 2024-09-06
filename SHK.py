b0ba9036531632417130306e2e20299c341a9cc9
Ssealevel <lztssealevel@gmail.com>
2024-09-06 17:40:53 +0800

    modify the way to read data, save result, calculate CCF, and plot. Test using 13808.

--- SHK.py
+++ SHK.py
@@ -3,6 +3,7 @@ import scipy
 from scipy.interpolate import interp1d
 from scipy.optimize import basinhopping
 import matplotlib.pyplot as plt
+import os
 
 # Constants according to Perdelwitz et al. 2021
 lam_K = 3933.66
@@ -19,21 +20,42 @@ grid_dir = './PFS_SHK_phoenix_grid/'
 verify_plot_dir = './verify_plot/'
 
 # Load the spectrum and blaze for order 1 and 2, then normalize the spectrum
-def read_norm_spectrum(spe_f, blaze_f):
+def read_norm_spectrum(spe_f, blaze_f, use_thar_w=False, thar_f=None, use_drop_nan_inf=True):
     data_spe = scipy.io.readsav(spe_f)
-    wK, speK = data_spe['w'][1], data_spe['sp'][1]
-    speKerr = np.sqrt(speK)
-    wH, speH = data_spe['w'][2], data_spe['sp'][2]
-    speHerr = np.sqrt(speH)
+    if use_thar_w:
+        data_thar = scipy.io.readsav(thar_f)
+        wK = data_thar['thar_ws'][1]
+        wH = data_thar['thar_ws'][2]
+        speK = data_spe['sp'][1]
+        speH = data_spe['sp'][2]
+        speKerr = np.sqrt(speK)
+        speHerr = np.sqrt(speH)
+    else:
+        try:
+            wK, speK = data_spe['sw'][1], data_spe['sp'][1]
+            wH, speH = data_spe['sw'][2], data_spe['sp'][2]
+        except:
+            wK, speK = data_spe['w'][1], data_spe['sp'][1]
+            wH, speH = data_spe['w'][2], data_spe['sp'][2]
+        speKerr = np.sqrt(speK)
+        speHerr = np.sqrt(speH)
 
     data_blaze = scipy.io.readsav(blaze_f)
-    blazeK = data_blaze['smnf'][1]
-    blazeH = data_blaze['smnf'][2]
+    try:
+        blazeK = data_blaze['smsp'][1]
+        blazeH = data_blaze['smsp'][2]
+    except:
+        blazeK = data_blaze['smnf'][1]
+        blazeH = data_blaze['smnf'][2]
 
     speK = speK / blazeK
     speH = speH / blazeH
     speKerr = speKerr / blazeK
     speHerr = speHerr / blazeH
+
+    if use_drop_nan_inf:
+        wK, speK, speKerr = drop_nan_inf(wK, speK, speKerr)
+        wH, speH, speHerr = drop_nan_inf(wH, speH, speHerr)
 
     return wK, speK, speKerr, wH, speH, speHerr
 
@@ -61,43 +83,44 @@ def neg_like_ccf(z, w, spe_ms, w_th, spe_th):
     # using CCF as the negative likelihood
     w_shift = w/(1.0+z)  # shifting observed onto theoretical
     # interp the theoretical spectrum onto the shifted, observed wavelength
-    spe_th_interp = np.interp(w_shift, w_th, spe_th)  
-    corr = np.sum((spe_th_interp - spe_ms)**2)
+    if np.max(w_shift) > np.max(w_th) or np.min(w_shift) < np.min(w_th):
+        return 100*len(spe_ms)
+    spe_th_interp = np.interp(w_shift, w_th, spe_th) 
+    # corr = np.sum((spe_th_interp - spe_ms)**2)
+    id_corr = np.where((w_shift < lam_H-1) | (w_shift > lam_H+1))[0]
+    corr = np.sum((spe_th_interp[id_corr] - spe_ms[id_corr])**2)
     return corr
 
-def verify_plot(w, spe_ms, w_th, spe_th, z_best, target):
-    plt.figure(figsize=(10, 6))
+def verify_plot_fit_z(w, spe_ms, w_th, spe_th, z_best, fig_name):
+    plt.figure(figsize=(12, 6), dpi=150)
     w_shifted = w / (1.0 + z_best)   
-    plt.plot(w_shifted, spe_ms, label="Shifted Observed Spectrum", color="blue")  #Plot the observed spectrum
-    plt.plot(w_th, spe_th, label=f"Theoretical Spectrum (z={z_best:.6f})", color="red") #Plot the shifted Observed spectrum
+    plt.plot(w_shifted, spe_ms, label=f"Shifted Observed Spectrum (z={z_best:.6f})", color="blue")  #Plot the observed spectrum
+    plt.plot(w_th, spe_th, label=f"Theoretical Spectrum", color="red") #Plot the shifted Observed spectrum
     plt.xlabel('Wavelength')
     plt.ylabel('Flux')
     plt.title('Theoretical vs Shifted Observed Spectrum')
     plt.legend()
-    plt.xlim((min(w), max(w)))
-    plt.savefig(verify_plot_dir + f'/verify_plot_z_{target}.png')  #Save the plot
+    plt.xlim((min(w_shifted), max(w_shifted)))
+    plt.savefig(verify_plot_dir + fig_name)
     plt.close()
 
 # Find the reshift of this observed spectrum using the model spec
-def fit_z(w, spe_ms, w_th, spe_th, init_z=0.0001, niter=1000, verify=True):
-    left_cut = 2000
-    right_cut = 9000
-    fit_w = w[left_cut:right_cut]
-    fit_spe_ms = spe_ms[left_cut:right_cut]
+def fit_z(w, spe_ms, w_th, spe_th, init_z=0.0001, niter=1000, 
+          verify=True, target='', filename=''):
+    
+    fit_w, fit_spe_ms = w[:], spe_ms[:] / np.percentile(spe_ms, 90.)
 
-    nan_ind = np.isnan(fit_spe_ms) | np.isinf(fit_spe_ms)
-    fit_w = fit_w[~nan_ind]
-    fit_spe_ms = fit_spe_ms[~nan_ind]
-    fit_spe_ms = fit_spe_ms / np.max(fit_spe_ms)
-
-    buffer = 2.0
-    obsw_max = np.max(fit_w) - buffer
-    obsw_min = np.min(fit_w) + buffer
+    buffer = 10.0
+    obsw_max = np.max(fit_w) + buffer
+    obsw_min = np.min(fit_w) - buffer
     ind_ref = np.where((w_th>obsw_min)&(w_th<obsw_max))[0]
     wv_ref = w_th[ind_ref]
     spe_ref = spe_th[ind_ref]
-    
-    # verify_plot(fit_w, fit_spe_ms, wv_ref, spe_ref, init_z, target)
+    # spe_ref = spe_ref / np.max(spe_ref)
+    spe_ref = spe_ref / np.percentile(spe_ref, 90.)
+
+    verify_plot_fit_z(fit_w, fit_spe_ms, wv_ref, spe_ref, init_z, 
+                      fig_name=f'{target}_{filename}_fit_z_verify_plot_z_{init_z:.6f}.png')
 
     minimizer_kwargs = {"method": "L-BFGS-B", "args":(fit_w, fit_spe_ms, wv_ref, spe_ref)}
     ret = basinhopping(neg_like_ccf, [init_z], minimizer_kwargs=minimizer_kwargs, niter=niter)
@@ -106,20 +129,29 @@ def fit_z(w, spe_ms, w_th, spe_th, init_z=0.0001, niter=1000, verify=True):
         z_best = ret.x[0]
         # Call verify_plot if verify is True
         if verify:
-            verify_plot(fit_w, fit_spe_ms, wv_ref, spe_ref, z_best, target)
+            verify_plot_fit_z(fit_w, fit_spe_ms, wv_ref, spe_ref, z_best,
+                              fig_name=f'{target}_{filename}_fit_z_verify_plot_z_{z_best:.6f}.png')
         return z_best  # z value best-fit
     else:
         print("Convergence on z failed!")
         if verify:
-            verify_plot(fit_w, fit_spe_ms, wv_ref, spe_ref, 0., target)
+            verify_plot_fit_z(fit_w, fit_spe_ms, wv_ref, spe_ref, 0.,
+                              fig_name=f'{target}_{filename}_fit_z_verify_plot_z_0.png')
         return 0.0
 
 # Shift the observed spectrum onto the model's wavelength spectrum
-def shift_wavelength(w_th, spe_th, w_tharK, spe_msK, w_tharH, spe_msH):
-    z = fit_z(w_tharH, spe_msH, w_th, spe_th)
+def shift_wavelength(w_th, spe_th, w_tharK, spe_msK, w_tharH, spe_msH,
+                     verify=True, target='', filename=''):
+    z = fit_z(w_tharH, spe_msH, w_th, spe_th, 
+              verify=verify, target=target, filename=filename)
     print(z) # for test
     w_shiftK = w_tharK / (1+z)
     w_shiftH = w_tharH / (1+z)
+    if verify:
+        verify_plot_fit_z(w_tharK, spe_msK/np.percentile(spe_msK,90), w_th, spe_th/np.percentile(spe_th,90), z,
+                        fig_name=f'{target}_{filename}_shifted_K_verify_plot.png')
+        verify_plot_fit_z(w_tharH, spe_msH/np.percentile(spe_msH,90), w_th, spe_th/np.percentile(spe_th,90), z,
+                        fig_name=f'{target}_{filename}_shifted_H_verify_plot.png')
     return w_shiftK, w_shiftH
 
 def drop_nan_inf(w, spe, spe_err):
@@ -151,8 +183,32 @@ def gradient(lam, f1, lam1, f2, lam2):
 def integral_flux(w, spe):
     return np.trapz(spe, w)
 
+def verify_plot_SHK_gradient(w, spe_th, spe_ms, lam_x, f1th, f1ms, lam_x1, f2th, f2ms, lam_x2, fig_name):
+    spe_rec = spe_ms * gradient(w, f1th, lam_x1, f2th, lam_x2) / gradient(w, f1ms, lam_x1, f2ms, lam_x2)
+    plt.figure(dpi=150, figsize=(12, 6))
+    plt.plot(w, spe_rec, label='Rectificated Observation')
+    plt.plot(w, spe_th, label='PHOENIX')
+    xlim = [lam_x1-5, lam_x2+5]
+    plt.xlim(xlim)
+    ind = (w >= xlim[0]) & (w <= xlim[1])
+    plt.ylim(0, 1.2*np.max(spe_th[ind]))
+    plt.axvline(lam_x-HW0, color='r', linestyle='--')
+    plt.axvline(lam_x+HW0, color='r', linestyle='--')
+    plt.axvline(lam_x1-HW12, color='b', linestyle='--')
+    plt.axvline(lam_x1+HW12, color='b', linestyle='--')
+    plt.axvline(lam_x2-HW12, color='b', linestyle='--')
+    plt.axvline(lam_x2+HW12, color='b', linestyle='--')
+    plt.xlabel('Wavelength')
+    plt.ylabel('Flux')
+    plt.title('Theoretical vs Rectification of the Observation')
+    plt.legend()
+    plt.savefig(verify_plot_dir + fig_name)
+    plt.close()
+    return
+
 # Calculate the emission flux for H or K line
-def S_x(w, spe_th, spe_flux, spe_ms, lam_x, lam_x1, lam_x2, HW0, HW12):
+def S_x(w, spe_th, spe_flux, spe_ms, lam_x, lam_x1, lam_x2, HW0, HW12,
+        verify=True, verify_plot_name=''):
     range_x, range_x1, range_x2 = [lam_x-HW0, lam_x+HW0], [lam_x1-HW12, lam_x1+HW12], [lam_x2-HW12, lam_x2+HW12]
     id_x = (w >= range_x[0]) & (w <= range_x[1])
     id_x1 = (w >= range_x1[0]) & (w <= range_x1[1])
@@ -162,17 +218,23 @@ def S_x(w, spe_th, spe_flux, spe_ms, lam_x, lam_x1, lam_x2, HW0, HW12):
     f1ms, f2ms = integral_flux(w[id_x1], spe_ms[id_x1]), integral_flux(w[id_x2], spe_ms[id_x2])
     spe_rec_x = spe_ms[id_x] * gradient(w[id_x], f1th, lam_x1, f2th, lam_x2) / gradient(w[id_x], f1ms, lam_x1, f2ms, lam_x2)
 
+    if verify:
+        verify_plot_SHK_gradient(w, spe_th, spe_ms, lam_x, f1th, f1ms, lam_x1, f2th, f2ms, lam_x2, verify_plot_name)
+
     Sx = integral_flux(w[id_x]*1e-8, (spe_rec_x - spe_th[id_x]) * spe_flux[id_x] / spe_th[id_x]) #w from A to cm, spe from normalized to real flux (cgs)
     return Sx
 
 def SHK_logRHK(Teff, wK, spethK, spefluxK, speK,
-            wH, spethH, spefluxH, speH):
+            wH, spethH, spefluxH, speH, 
+            verify=False, target='', filename=''):
     '''
     Calculate SHK and RHK
     Now the SHK is only the emission flux of H and K line.
     '''
-    S_K = S_x(wK, spethK, spefluxK, speK, lam_K, lam_k1, lam_k2, HW0, HW12)
-    S_H = S_x(wH, spethH, spefluxH, speH, lam_H, lam_h1, lam_h2, HW0, HW12)
+    S_K = S_x(wK, spethK, spefluxK, speK, lam_K, lam_k1, lam_k2, HW0, HW12,
+              verify=verify, verify_plot_name=f'{target}_{filename}_SK_Rectification_verify_plot.png')
+    S_H = S_x(wH, spethH, spefluxH, speH, lam_H, lam_h1, lam_h2, HW0, HW12,
+              verify=verify, verify_plot_name=f'{target}_{filename}_SH_Rectification_verify_plot.png')
 
     SHK = S_H + S_K
     RHK = (S_H + S_K) / (sigma_SB * Teff**4)
@@ -189,7 +251,7 @@ def SHK_logRHK_err_MC(Teff, wK, spethK, spefluxK, speK, speKerr,
     Using Monte Carlo approach to estimate the error of SHK and RHK.
     Do not consider the error of the stellar parameters now.
     '''
-    SHK, logRHK = SHK_logRHK(Teff, wK, spethK, spefluxK, speK, wH, spethH, speH, spefluxH)
+    SHK, logRHK = SHK_logRHK(Teff, wK, spethK, spefluxK, speK, wH, spethH, spefluxH, speH)
     SHK_samples, logRHK_samples = [], []
     for n in range(N_sim):
         speK_sim = np.random.normal(speK, speKerr)
@@ -201,36 +263,56 @@ def SHK_logRHK_err_MC(Teff, wK, spethK, spefluxK, speK, speKerr,
     SHK_samples, logRHK_samples = np.array(SHK_samples), np.array(logRHK_samples)
     SHK_uerr, SHK_lerr = errorbar(SHK_samples)
     logRHK_uerr, logRHK_lerr = errorbar(logRHK_samples)
-    return [SHK, SHK_lerr, SHK_uerr], [logRHK, logRHK_lerr, logRHK_uerr]
-    # return [np.median(SHK_samples), SHK_lerr, SHK_uerr], [np.median(logRHK_samples), logRHK_lerr, logRHK_uerr]
+    # return [SHK, SHK_lerr, SHK_uerr], [logRHK, logRHK_lerr, logRHK_uerr]
+    return [np.median(SHK_samples), SHK_lerr, SHK_uerr], [np.median(logRHK_samples), logRHK_lerr, logRHK_uerr]
 
 
 if __name__ == '__main__':
     # TODO: change the inputs
-    target = 'hd13808'
     # target = 'hd20155'
     # target = 'hip27323'
-    spe_f = f'./test_data/spectra/{target}.dat'
-    blaze_f = f'./test_data/blaze/nf_n66_15.dat'
-    thar_f = f'./test_data/tharws/n66.4226.tharws.sav'
+
+    target = 'hd13808'
+    spe_fs = [f'./test_data/spectra/{target}.dat']
+    blaze_f = f'./test_data/blaze/nf_n66_15.dat' # hd13808
+    thar_f = f'./test_data/tharws/n66.4226.tharws.sav' # hd13808
     Teff = 5002
 
-    wK, speK, speKerr, wH, speH, speHerr = read_norm_spectrum(spe_f, blaze_f)
-    w_th, spe_th, spe_flux = read_theoretical_spectrum(Teff)
-    w_tharK, w_tharH = read_thar_wavelength(thar_f)
-    wK_shift, wH_shift = shift_wavelength(w_th, spe_th, w_tharK, speK, w_tharH, speH)
-    wK_new, speK_new, speKerr_new = drop_nan_inf(wK_shift, speK, speKerr)
-    wH_new, speH_new, speHerr_new = drop_nan_inf(wH_shift, speH, speHerr)
-    spethK_new, spefluxK_new, spethH_new, spefluxH_new = intepolate_speth2wms(wK_new, wH_new, w_th, spe_th, spe_flux)
+    # target = 'hd197481' # AU Mic
+    # # spe_f = f'./test_data/spectra/197481_data/rn68.2630'
+    # spe_dir = './test_data/spectra/197481_data/'
+    # blaze_f = f'./test_data/blaze/nf_n68_11.dat' # AU Mic (hd197481)
+    # thar_f = f'./test_data/tharws/n68.2694.tharws.sav' # AU Mic (hd197481)
+    # Teff = 3688
+    # spe_fs = [spe_dir + f'rn68.{n}' for n in range(2631,2676)]
 
-    # Change how to output the results
-    SHK, logRHK = SHK_logRHK(Teff, wK_new, spethK_new, spefluxK_new, speK_new,
-                             wH_new, spethH_new, spefluxH_new, speH_new)
-    print(SHK, logRHK)
+    results = []
+    for spe_f in spe_fs:
+        wK, speK, speKerr, wH, speH, speHerr = \
+            read_norm_spectrum(spe_f, blaze_f, use_thar_w=True, thar_f=thar_f, use_drop_nan_inf=True)
+        w_th, spe_th, spe_flux = read_theoretical_spectrum(Teff)
+        wK_shift, wH_shift = shift_wavelength(w_th, spe_th, wK, speK, wH, speH,
+                                            verify=True, target=target, filename=os.path.basename(spe_f))
+        wK_new, speK_new, speKerr_new = drop_nan_inf(wK_shift, speK, speKerr)
+        wH_new, speH_new, speHerr_new = drop_nan_inf(wH_shift, speH, speHerr)
+        spethK_new, spefluxK_new, spethH_new, spefluxH_new = \
+            intepolate_speth2wms(wK_new, wH_new, w_th, spe_th, spe_flux)
 
-    SHKs, logRHKs = SHK_logRHK_err_MC(Teff, wK_new, spethK_new, spefluxK_new, speK_new, speKerr_new, 
-                                      wH_new, spethH_new, spefluxH_new, speH_new, speHerr_new)
-    print('SHK:')
-    print(SHKs)
-    print('logRHK:')
-    print(logRHKs)
+        # Change how to output the results
+        SHK, logRHK = SHK_logRHK(Teff, wK_new, spethK_new, spefluxK_new, speK_new,
+                                wH_new, spethH_new, spefluxH_new, speH_new,
+                                verify=True, target=target, filename=os.path.basename(spe_f))
+        print(SHK, logRHK)
+
+        SHKs, logRHKs = SHK_logRHK_err_MC(Teff, wK_new, spethK_new, spefluxK_new, speK_new, speKerr_new, 
+                                        wH_new, spethH_new, spefluxH_new, speH_new, speHerr_new)
+        print('SHK:')
+        print(SHKs)
+        print('logRHK:')
+        print(logRHKs)
+
+        results.append(np.hstack((SHKs, logRHKs)))
+    
+    results = np.array(results)
+    np.savetxt(f'./{target}_SHK_logRHK_results.txt', results, fmt='%.6f',
+               header='SHK, SHK_lerr, SHK_uerr, logRHK, logRHK_lerr, logRHK_uerr')
